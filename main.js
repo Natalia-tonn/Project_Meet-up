45a847da2b61902bad968eb0e8a17bd0c10c177e
Natalia-tonn <natali-tonn@web.de>
2024-07-09 14:04:20 +0200

    added html

---
+++ main.js
@@ -0,0 +1,5 @@
+const joinMeetup = document.querySelector(".join Meetup")
+
+joinMeetup.addEventListener("click", () => {
+    console.log(location.pathname = "/page2.html")
+})
---
+++ main2.js
@@ -0,0 +1,8 @@
+
+
+const goMain = document.querySelector(".go Main")
+
+
+goMain.addEventListener("click", () => {
+    console.log(location.pathname = "/index.html")
+})
